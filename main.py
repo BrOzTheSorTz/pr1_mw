b8be5c8dabe5da6677a6171a8acb1957db2d658b
Jose Pablo Soriano Torres <jpst0002@red.ujaen.es>
2025-03-11 17:18:38 +0100

    [ADD] Last changes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from src.data_collection.spotify_api import SpotifyClient
 from src.data_collection.data_scraper import DataScraper
+from src.utils.config import nodes_path,edges_path
+
+def crear_grafo(max_artist,max_deep,name_artist="Melendi"):
+    spotify = SpotifyClient()
+    scrapper = DataScraper(spotify,max_artist,max_deep)
+    scrapper.search_collaborator_deep(name_artist)
 
 
 if __name__ == "__main__":
 
-    spotify = SpotifyClient()
-    max_artist = 1500
-    max_deep = 2
-    scrapper = DataScraper(spotify,max_artist,max_deep)
-
-    scrapper.search_collaborator_deep("Melendi")
-
-
-
+    crear_grafo(1500,2,"Melendi")
